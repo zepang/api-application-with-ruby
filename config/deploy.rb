a895468a7c4ab8c83871aaf21b34b936023c1abe
ze-pang <ummlqzp@gmail.com>
2018-04-21 22:09:25 +0800

    修改数据库配置

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,7 @@ set :port, 22           # SSH port number.
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
 # set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('.env')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
